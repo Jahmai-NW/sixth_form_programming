d2e73b2aa76075ac3cf8bac545d0c1d1b6941a5b
unknown <2525Nandoh-WilliamsJ@centralfoundationboys.co.uk>
2025-10-31 18:22:24 +0000

    halloween

--- classwork_or_progbytes/testing_pb.py
+++ classwork_or_progbytes/testing_pb.py
@@ -1,5 +1,7 @@
-number = int(input("Enter a number: "))
-if number % 2 == 0:
-    print("Even")
-else:
-    print("Odd")
+def convert_temperature(celsius):
+    fahrenheit = (celsius * 9/5) + 32
+    print(celsius, "°C is equal to", fahrenheit, "F")
+
+
+temp = float(input("Enter temperature in Celsius: "))
+convert_temperature(temp)
