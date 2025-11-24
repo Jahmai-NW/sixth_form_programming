050f3aeaeded53d7eea9d9822be000d945f22ef6
Jamhmai Nandoh-Williams <25Nandoh-WilliamsJ@CFBS.Domain.Local>
2025-11-24 12:15:28 +0000

    task 1 table

--- classwork_or_progbytes/cheese.py
+++ classwork_or_progbytes/cheese.py
@@ -1,8 +1,17 @@
-total = 0
-num = int(input("Enter Number: "))
-while num > 0:
-    total += num
-    num = int(input("Enter Number: "))
-print("Total Number:", total)
+def multiples(table, startnum, endnum, pupilName):
+    print("Hi, " + pupilName + "...here is your times table")
+    #product = table * i
+    for i in range(startnum,endnum+1):
+        product = table * i
+        print(str(table) + " x " + str(i) + " = " + str(product))
+
+        
+    
+pupilName = input("What is your name?:  ")
+table = int(input("Enter times table: "))
+startnum = int(input("Enter start number: "))
+endnum = int(input("Enter end number: "))
+
+multiples(table, startnum, endnum, pupilName)
 
 
