7c7d59fb1f2b343467e7a08184173d9780a1e29c
Jamhmai Nandoh-Williams <25Nandoh-WilliamsJ@CFBS.Domain.Local>
2026-02-04 09:47:23 +0000

    04/02/2026

--- classwork_or_progbytes/linkedList.py
+++ classwork_or_progbytes/linkedList.py
@@ -1,5 +1,8 @@
+########## LINKED LIST CLASS ###########
+
 class LinkedList():
-    def __init__(self,headData):
+    # constructor
+    def __init__(self, headData):
         self.head = Node(headData)
 
     def getHead(self):
@@ -7,32 +10,30 @@ class LinkedList():
     
     def add(self, newData):
         return None
-    
 
-
+########## NODE CLASS #################
 class Node():
-    def __init__(self, theData):
-        self.data = theData
+    # constructor
+    def __init__(self,newData):
+        self.data = newData
         self.next = None
 
-#####getters
     def getData(self):
         return self.data
-    
-    def setData(self, data):
-        self.data = data
     
     def getNext(self):
         return self.next
     
-    def setNext(self, next):
-        self.next = next
-        
+    def setNext(self, newNext):
+        self.next = newNext
 
-################# MAIN PROGRAM #########################
+#######################################
+# MAIN PROGRAM
+#######################################
 
-firstPerson = input("Who is the first person in your list?: ")
+firstPerson = input("What is the first person in your list?: ")
 myList = LinkedList(firstPerson)
+
 
 print(myList)
 print(myList.getHead())
