c9abeff612c1f4ce6252fe013dee3469625df970
Jamhmai Nandoh-Williams <25Nandoh-WilliamsJ@CFBS.Domain.Local>
2026-04-24 10:59:17 +0100

    24/04/2026

--- OOP projects/bank.py
+++ OOP projects/bank.py
@@ -1,8 +1,72 @@
-class Account():
-    def __init__(self, theBalance):
-        self.balance = theBalance
+class BankAccount():
+    def __init__(self, theAccountHolder, initialBalance=0):
+        self.accountholder = theAccountHolder
+        self.balance = initialBalance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.balance += amount
+            print(amount, "has been deposited.")
+            print("New Balance:", self.balance)
+            return True
+        else:
+            return False
+    
+    def withdraw(self, amount):
+        if amount > self.balance:
+            print("Insufficient funds.")
+        else:
+            self.balance -= amount
+            print("Withdrew", amount)
+            print("New Balance:", self.balance)
+
+    def check_balance(self):
+        print("Account Holder:", self.accountholder)
+        print("Current Balance:", self.balance)
 
 
 class Bank():
-    def __init__(self, theTotal):
-        self.total = theTotal
+    def __init__(self, theAccounts):
+        self.accounts = theAccounts
+    
+    # def createAccount(self, theAccountHolder, ):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+###########################TESTING##############################
+while True:
+    print('''
+                                Menu:
+    Please choose an option below by entering the number accordingly.
+    1. Check Current Balance
+    2. Deposit
+    3. Withdraw
+    4. Create a new account
+    5. Exit''')
+
+
+
+my_account = BankAccount("Alice", 100)
+my_account.check_balance()
