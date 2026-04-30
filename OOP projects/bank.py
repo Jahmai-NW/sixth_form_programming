5af7fad944f6bcaa5c598a81071ce2c09e0d6f89
Jamhmai Nandoh-Williams <25Nandoh-WilliamsJ@CFBS.Domain.Local>
2026-04-30 14:57:41 +0100

    30/04/2026

--- OOP projects/bank.py
+++ OOP projects/bank.py
@@ -24,10 +24,17 @@ class BankAccount():
         print("Account Holder:", self.accountholder)
         print("Current Balance:", self.balance)
 
+    
+
 
 class Bank():
-    def __init__(self, theAccounts):
-        self.accounts = theAccounts
+    def __init__(self):
+        # self.accounts = []
+        self.bankTotal = 100000
+
+    def subtract(self, amount):
+        self.bankTotal -= amount
+        return self.bankTotal
     
     # def createAccount(self, theAccountHolder, ):
 
@@ -35,6 +42,15 @@ class Bank():
 
 
 
+def option1():
+    enterName = input("Please enter your full name: ")
+    Account = BankAccount(enterName, 0)
+    print("Your account has been created.")
+    
+    
+def option4():
+    enterName = input("Please enter your full name: ")
+    if Account.
 
 
 
@@ -42,31 +58,47 @@ class Bank():
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-###########################TESTING##############################
+# ###########################TESTING##############################
 while True:
     print('''
-                                Menu:
+    ----------------------------------------------------------------------------
+                                Menu
     Please choose an option below by entering the number accordingly.
-    1. Check Current Balance
+    1. Create a new account
     2. Deposit
     3. Withdraw
-    4. Create a new account
-    5. Exit''')
+    4. Check Current Balance
+    5. Exit
+    ----------------------------------------------------------------------------''')
+
+    choice = int(input("Enter Number Here: "))
+
+    if choice == 1:
+        option1()
+
+    elif choice == 2:
+        option2()
+
+    elif choice == 3:
+        option3()
+
+    elif choice == 4:
+        option4()
+
+    elif choice == 5:
+        exit()
+    
+    elif choice < 1:
+        print("Invalid option")
+         
+
+    
 
 
 
-my_account = BankAccount("Alice", 100)
-my_account.check_balance()
+# my_account = BankAccount("Alice", 100)
+# my_account.check_balance()
+# bank1 = Bank()
+
+# my_account.withdraw(50)
+# print(bank1.subtract(50))
--- classwork_or_progbytes/cheese.py
+++ classwork_or_progbytes/cheese.py
@@ -1,17 +1,3 @@
-def multiples(table, startnum, endnum, pupilName):
-    print("Hi, " + pupilName + "...here is your times table")
-    #product = table * i
-    for i in range(startnum,endnum+1):
-        product = table * i
-        print(str(table) + " x " + str(i) + " = " + str(product))
-
-        
-    
-pupilName = input("What is your name?:  ")
-table = int(input("Enter times table: "))
-startnum = int(input("Enter start number: "))
-endnum = int(input("Enter end number: "))
-
-multiples(table, startnum, endnum, pupilName)
+print(1 % 5)
 
 
