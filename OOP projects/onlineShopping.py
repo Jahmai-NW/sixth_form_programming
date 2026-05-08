ba1ee38de8853183c97424f26f54d62b61adf3a7
Jamhmai Nandoh-Williams <25Nandoh-WilliamsJ@CFBS.Domain.Local>
2026-05-08 11:41:38 +0100

    08/05/2026

--- OOP projects/onlineShopping.py
+++ OOP projects/onlineShopping.py
@@ -1,13 +1,19 @@
 class Customer():
     def __init__(self, thePersonalDetails, theAccountBalance):
         self.personalDetails = thePersonalDetails
-        self.accountBalance = theAccountBalance
+        self.__accountBalance = theAccountBalance
 
     def getPersonalDetails(self):
         print(f"Here is your ID: {self.personalDetails}")
 
     def getAccountBalance(self):
-        print(f"Here is your Bal: {self.accountBalance}")
+        print(f"Here is your current balance: {self.__accountBalance}")
+
+    def updateBalance(self, amount):
+        if self.__accountBalance + amount < 0:
+            print("Insufficient funds.")
+        else:
+            self.__accountBalance += amount
 
 ####
 
@@ -23,13 +29,75 @@ class Products():
         print(f"This is a {self.items}")
 
     
+def showMenu():
+    print('''                                           MENU
+    --------------------------------------------------------------------------------------------------------
+                                    1. Create an account
+                                    2. Check balance
+                                    3. Update balance
+                                    4. View available items
+                                    5. Make a purchase
+                                    6. Exit the store
+    ''')
+
+
+
+
+from abc import ABC, abstractmethod
+
+class Purchase(ABC):
+    @abstractmethod
+    def processPurchase(self, customer, amount):
+        pass
+
+class CreditCardPayment(Purchase):
+    def processPurchase(self, customer, amount):
+        print(f"Processing credit card payment of £{amount} for {customer.personalDetails}")
+        customer.updateBalance(-amount)
 
 
 
 
 
 
-####
+#######################################################################
+showMenu()
+item1 = Products("Inverted Spear of Heaven", 100)
+item2 = Products("Hollow Purple", 200)
+item3 = Products("Domain Expansion: Infinite Void", 450)
+
+
+while True:
+    choice = int(input("Please enter the number of your required service: "))
+
+    if choice == 1:
+        name = input("Please enter your name: ")
+        startingBal = int(input("Please enter your starting balance as an integer: "))
+        customer = Customer(name, startingBal)
+        print("")
+        print("Your account has been created successfully.")
+        print("")
+        showMenu()
+
+
+    if choice == 2:
+        print(customer.getAccountBalance())
+        print("")
+        showMenu()
+
+    if choice == 3:
+        None
+
+    if choice == 4:
+        None
+
+    if choice == 5:
+        None
+
+    if choice == 6: 
+        None
+        
+    
 
 
 
@@ -38,28 +106,6 @@ class Products():
 
 
 
-
-
-
-class Purchases(Customer, Products): #Child Class
-    def __init__(self, thePersonalDetails, theAccountBalance, theItems, thePrices):
-        super().__init__(thePersonalDetails, theAccountBalance)
-        super().__init__(theItems, thePrices)
-
-    def finalPurchase(self):    
-        getPersonalDetails()
-        getItem()
-
-
-    def finalInformation(self):
-        requiredQuantity()
-        newBalance = self.accountBalance - totalPrice
-        print(f"Your new balance is now {newBalance}")
-
-    def requiredQuantity(self, quantity):
-        quantity = int(input(f"How many {self.items}s would you like to purchase?: "))
-        totalPrice = quantity * self.prices
-        return totalPrice
 
 
 
