75d4213a4eec93f279cd48e60277be8412b816c3
Jamhmai Nandoh-Williams <25Nandoh-WilliamsJ@CFBS.Domain.Local>
2026-05-08 12:11:28 +0100

    purchasing

--- OOP projects/onlineShopping.py
+++ OOP projects/onlineShopping.py
@@ -26,7 +26,7 @@ class Products():
         print(f"This item costs {self.prices}")
 
     def getItem(self):
-        print(f"This is a {self.items}")
+        print(f"{self.items}")
 
     
 def showMenu():
@@ -65,6 +65,16 @@ showMenu()
 item1 = Products("Inverted Spear of Heaven", 100)
 item2 = Products("Hollow Purple", 200)
 item3 = Products("Domain Expansion: Infinite Void", 450)
+item4 = Products("Strand of All Might's Hair", 500)
+item5 = Products("Domain Expansion: Malevolent Shrine", 400)
+item6 = Products("Dismantle", 150)
+item7 = Products("Summon Rika", 700)
+item8 = Products("Domain Expansion: Womb Profusion", 425)
+item9 = Products("Domain Expansion: Chimera Shadow Garden", 350)
+item10 = Products("Become a Cursed Spirit", 900)
+
+def showAllItems():
+    collectionOfItems = [item1.getItem(), item2.getItem(), item3.getItem(), item4.getItem(), item5.getItem(), item6.getItem(), item7.getItem(), item8.getItem(), item9.getItem(), item10.getItem()]
 
 
 while True:
@@ -73,7 +83,7 @@ while True:
     if choice == 1:
         name = input("Please enter your name: ")
         startingBal = int(input("Please enter your starting balance as an integer: "))
-        customer = Customer(name, startingBal)
+        customer1 = Customer(name, startingBal)
         print("")
         print("Your account has been created successfully.")
         print("")
@@ -81,7 +91,7 @@ while True:
 
 
     if choice == 2:
-        print(customer.getAccountBalance())
+        print(customer1.getAccountBalance())
         print("")
         showMenu()
 
@@ -89,10 +99,29 @@ while True:
         None
 
     if choice == 4:
-        None
+        showAllItems()
+        print("")
+        showMenu()
 
     if choice == 5:
-        None
+        showAllItems()
+        chosenitem = input("Which item would you like to purchase?: ")
+
+        if chosenitem == item1.getItem():
+            if item1.getPrice <= customer1.getAccountBalance:
+                paymentMethod = CreditCardPayment()
+                paymentMethod.processPurchase(customer1, item1.getPrice)
+                print(customer1.getPersonalDetails(), "has bought", item1.getItem(), "for", item1.getPrice())
+                print("Current Balance:", customer1.getAccountBalance)
+            else:
+                print("Not enough balance to buy", item1.getItem())
+    
+
+
+
+
+            # or item2.getItem() or item3.getItem() or item4.getItem() or item5.getItem() or item6.getItem() or item7.getItem() or item8.getItem() or item9.getItem() or item10.getItem(): 
+            
 
     if choice == 6: 
         None
