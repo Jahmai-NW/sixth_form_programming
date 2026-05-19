42fc36047ff008e3d08cadbdc4f5a4a77db55148
Jamhmai Nandoh-Williams <25Nandoh-WilliamsJ@CFBS.Domain.Local>
2026-05-19 13:01:14 +0100

    domain clashing pt 1

--- OOP projects/shibuya_Incident.py
+++ OOP projects/shibuya_Incident.py
@@ -18,7 +18,7 @@
 
 
 class Sorcerers():
-    def __init__(self, theName, theHP, theAttack, theSpecialMove, theRankName, theDomainExpansionName):
+    def __init__(self, theName, theHP, theAttack, theSpecialMove, theRankName, theDomainExpansionName, theDomainValue, theSureHit):
         self.name = theName
         self.hp = theHP
         self.attack = theAttack
@@ -29,6 +29,8 @@ class Sorcerers():
         self.simpleDomain = False
         self.rankName = theRankName
         self.rankPoints = 0
+        self.domainValue = theDomainValue
+        self.sureHit = theSureHit
 
     def getName(self):
         return self.name
@@ -69,10 +71,22 @@ class Sorcerers():
     def setRankPoints(self, amount):
         self.rankPoints = amount
 
+    def getDomainValue(self):
+        return self.domainValue
+
+    def setDomainValue(self, amount):
+        self.domainValue = amount
+    
+    def getSureHit(self):
+        return self.sureHit
+
+    def setSureHit(self, newHit):
+        self.sureHit = newHit
+
     
 
 class CursedSpirit():
-    def __init__(self, theName, theHP, theAttack, theSpecialMove, theRankName, theDomainExpansionName):
+    def __init__(self, theName, theHP, theAttack, theSpecialMove, theRankName, theDomainExpansionName, theDomainValue, theSureHit):
         self.name = theName
         self.hp = theHP
         self.attack = theAttack
@@ -82,6 +96,8 @@ class CursedSpirit():
         self.domainExpansion = False
         self.rankName = theRankName
         self.rankPoints = 0
+        self.domainValue = theDomainValue
+        self.sureHit = theSureHit
 
     def getName(self):
         return self.name
@@ -119,33 +135,48 @@ class CursedSpirit():
     def setRankPoints(self, amount):
         self.rankPoints = amount
 
+    def getDomainValue(self):
+        return self.domainValue
+
+    def setDomainValue(self, amount):
+        self.domainValue = amount
+
+    def getSureHit(self):
+        return self.sureHit
+
+    def setSureHit(self, newHit):
+        self.sureHit = newHit
+
+
 #max hp = 10000
 #min hp = 1
 
 #max attack = 10000
 #min attack = 1
 
+#max domainvalue = 500
+#min domainvalue = 0
 
-YujiItadori = Sorcerers("Yuji Itadori", 3000, 1000, "Black Flash", "Special Grade", "Benevolent Shrine")
-YutaOkkotsu = Sorcerers("Yuta Okkotsu", 8000, 8000, "Summon Rika", "Special Grade", "Authentic Mutual Love")
-SatoruGojo = Sorcerers("Satoru Gojo", 9500, 9500, "200% Hollow Purple", "Special Grade", "Infinite Void")
-RyomenSukuna = Sorcerers("Ryomen Sukuna", 10000, 10000, "World Cutting Slash", "Special Grade", "Malevolent Shrine")
-YukiTsukumo = Sorcerers("Yuki Tsukumo", 7000, 7000, "Black Hole", "Special Grade", "Star Rage")
-SuguruGeto = Sorcerers("Suguru Geto", 1500, 1000, "Uzumaki", "Special Grade", "Womb Profusion")
-Kenjaku = Sorcerers("Kenjaku", 1500, 6000, "Anti-Gravity", "Special Grade", "Womb Profusion")
-MakiZenin = Sorcerers("Maki Zenin", 2500, 500, "Inhumane Speed", "Heavenly Restricted", None)
-MegumiFushiguro = Sorcerers("Megumi Fushiguro", 1000, 9000, "Summon Mahoraga", "Grade 2", "Chimera Shadow Garden")
-TojiFushiguro = Sorcerers("Toji Fushiguro", 2250, 700, "Sneak Attack", "Heavenly Restricted", None)
-NaoyaZenin = Sorcerers("Naoya Zenin", 700, 500, "Enforce 24FPS", "Special Grade 1", None)
-KinjiHakari = Sorcerers("Kinji Hakari", 6500, 3000, "Instantaneous Regeneration", "Grade 1", "Idle Death Gamble")
-HajimeKashimo = Sorcerers("Hajime Kashimo", 7800, 7800, "Mythical Beast Amber", "Special Grade", None)
+YujiItadori = Sorcerers("Yuji Itadori", 3000, 1000, "Black Flash", "Special Grade", "Benevolent Shrine", 200, "gets hit by soul dismantles.")
+YutaOkkotsu = Sorcerers("Yuta Okkotsu", 8000, 8000, "Summon Rika", "Special Grade", "Authentic Mutual Love", 400, "is hit by a technique from Yuta's sword.")
+SatoruGojo = Sorcerers("Satoru Gojo", 9500, 9500, "200% Hollow Purple", "Special Grade", "Infinite Void", 480, "is forced to bear the weight of boundless information.")
+RyomenSukuna = Sorcerers("Ryomen Sukuna", 10000, 10000, "World Cutting Slash", "Special Grade", "Malevolent Shrine", 500, "is hit by endless dismantle and cleave attacks.")
+YukiTsukumo = Sorcerers("Yuki Tsukumo", 7000, 7000, "Black Hole", "Special Grade", "Star Rage", 420, "is hit by the effects of infinite mass.")
+SuguruGeto = Sorcerers("Suguru Geto", 1500, 1000, "Uzumaki", "Special Grade", "Womb Profusion", 20, "is hit by the onslaught of Cursed Spirits.")
+Kenjaku = Sorcerers("Kenjaku", 1500, 6000, "Anti-Gravity", "Special Grade", "Womb Profusion", 490, "is immediately crushed.")
+MakiZenin = Sorcerers("Maki Zenin", 2500, 500, "Inhumane Speed", "Heavenly Restricted", None, 0, "invades the domain.")
+MegumiFushiguro = Sorcerers("Megumi Fushiguro", 1000, 9000, "Summon Mahoraga", "Grade 2", "Chimera Shadow Garden", 250, "is attacked bu multiple shadows.")
+TojiFushiguro = Sorcerers("Toji Fushiguro", 2250, 700, "Sneak Attack", "Heavenly Restricted", None, 0, "invades the domain.")
+NaoyaZenin = Sorcerers("Naoya Zenin", 700, 500, "Enforce 24FPS", "Special Grade 1", None, 0, "is safe.")
+KinjiHakari = Sorcerers("Kinji Hakari", 6500, 3000, "Instantaneous Regeneration", "Grade 1", "Idle Death Gamble", 390, "is severely injured by Jackpot Hakari.")
+HajimeKashimo = Sorcerers("Hajime Kashimo", 7800, 7800, "Mythical Beast Amber", "Special Grade", None, 0, "is safe.")
 
 
-Mahito = CursedSpirit("Mahito", 6000, 4000, "Embodiment of Perfection", "Special Grade", "Self-Embodiment Of Perfection")
-Jogo = CursedSpirit("Jogo", 1500, 3000, "Maximum Meteor", "Special Grade", "Coffin of the Iron Mountain")
-Dagon = CursedSpirit("Dagon", 1000, 1500, "Awakening Form", "Special Grade", "Horizon of the Captivating Skandha")
-Choso = CursedSpirit("Choso", 2000, 2000, "Supernova", "Grade 1", None)
-Naoya = CursedSpirit("Cursed Spirit Naoya", 4000, 4000, "Apply 24FPS on Air", "Special Grade", "Time Cell Moon Palace")
+Mahito = CursedSpirit("Mahito", 6000, 4000, "Embodiment of Perfection", "Special Grade", "Self-Embodiment Of Perfection", 450, "is forcefully morphed, as a result of Idle Transfiguration.")
+Jogo = CursedSpirit("Jogo", 1500, 3000, "Maximum Meteor", "Special Grade", "Coffin of the Iron Mountain", 425, "is severely burned.")
+Dagon = CursedSpirit("Dagon", 1000, 1500, "Awakening Form", "Special Grade", "Horizon of the Captivating Skandha", 400, "is attacked by sea creatures.")
+Choso = CursedSpirit("Choso", 2000, 2000, "Supernova", "Grade 1", None, 0, "is safe.")
+Naoya = CursedSpirit("Cursed Spirit Naoya", 4000, 4000, "Apply 24FPS on Air", "Special Grade", "Time Cell Moon Palace", 470, "fails to abide by Naoya's 24FPS rule. Their cells are shredded and torn apart.")
 
 TotalSorcerers = [YujiItadori, YutaOkkotsu, SatoruGojo, RyomenSukuna, YukiTsukumo, SuguruGeto, Kenjaku, MakiZenin, MegumiFushiguro, TojiFushiguro, NaoyaZenin, KinjiHakari, HajimeKashimo]
 TotalSpirits = [Mahito, Jogo, Dagon, Choso, Naoya]
@@ -165,13 +196,69 @@ def moreAttack(fighter1, fighter2):
 
 
 
-def fightingAnnounce():
+def fightingAnnounceSor():
     from random import choice
     fighter1 = choice(TotalSorcerers)
     fighter2 = choice(TotalSorcerers)
     if fighter1.getName() != fighter2.getName():
+        print("")
         print(fighter1.getName(), "vs", fighter2.getName())
         moreAttack(fighter1, fighter2)
+        print("")
 
 
-fightingAnnounce()
+def fightingAnnounceCur():
+    from random import choice
+    fighter1 = choice(TotalSpirits)
+    fighter2 = choice(TotalSpirits)
+    if fighter1.getName() != fighter2.getName():
+        print("")
+        print(fighter1.getName(), "vs", fighter2.getName())
+        moreAttack(fighter1, fighter2)
+        print("")
+
+
+def fightingAnnounceBoth():
+    from random import choice
+    fighter1 = choice(TotalSorcerers)
+    fighter2 = choice(TotalSpirits)
+    if fighter1.getName() != fighter2.getName():
+        print("")
+        print(fighter1.getName(), "vs", fighter2.getName())
+        moreAttack(fighter1, fighter2)
+        print("")
+
+def domainClash():
+    from random import choice
+    fighter1 = choice(TotalSorcerers)
+    fighter2 = choice(TotalSpirits)
+    if fighter1.getName() != fighter2.getName():
+        print("")
+        print(fighter1.getName(), "expands", fighter1.getTheDomainExpansionName(), "and", fighter2.getName(), "expands", fighter2.getTheDomainExpansionName())
+        print("")
+
+        if fighter1.getDomainValue() > fighter2.getDomainValue():
+            print(fighter1.getTheDomainExpansionName(), "is more refined and wins the clash.", fighter2.getName(), fighter1.getSureHit())
+
+
+while True:
+    print('''
+        1. Sorcerer vs Sorcerer
+        2. Cursed Spirit vs Cursed Spirit
+        3. Sorcerer vs Cursed Spirit
+        4. Domain Clash between Sorcerers and Cursed Spirits
+    ''')
+
+    option = int(input("Please enter your choice: "))
+
+    if option == 1:
+        fightingAnnounceSor()
+
+    if option == 2:
+        fightingAnnounceCur()
+
+    if option == 3:
+        fightingAnnounceBoth()
+
+    if option == 4:
+        domainClash()
