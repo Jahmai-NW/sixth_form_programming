e0c021ace6ba2b3e9249f0925e6ba1544d7ed33a
unknown <2525Nandoh-WilliamsJ@centralfoundationboys.co.uk>
2026-09-03 18:29:07 +0100

    beginning to layout code of game

---
+++ pygame_testing/project.py
@@ -0,0 +1,76 @@
+import pygame
+
+pygame.init()
+WIDTH, HEIGHT = 800, 600
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Penalty Shootout")
+clock = pygame.time.Clock()
+
+
+
+
+class Player():
+    #private username
+    #private HighScore
+None
+
+
+class PenaltyTaker():
+    #private background
+    #private totalScore
+    #private shotButton
+    #private shotDirection
+
+class Goalkeeper():
+    #private background
+    #private totalScore
+    #private diveButton
+    #private diveDirection
+
+
+class MainGame():
+    #private buttons (to track placement of buttons on the screen)
+    #private background
+    #private totalScore
+    #private 
+None
+
+
+class Button():
+    #private text
+    #private x
+    #private y
+    #private width
+    #private height
+None
+
+
+
+
+class MainMenuPanel():
+    #private buttons
+    #private background
+
+
+
+class SettingsPanel():
+    #private buttons
+    #private background
+
+
+class CustomisationPanel():
+    #private buttons
+    #private background
+
+class InstructionsPanel():
+    #private buttons
+    #private background
+
+
+class ViewScorePanel():
+    #private buttons
+    #private background
+
+
+
+pygame.quit()
