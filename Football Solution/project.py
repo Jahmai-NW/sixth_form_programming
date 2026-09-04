a07e8c3296b765351044eb56a0d1851b9b06a654
Jamhmai Nandoh-Williams <25Nandoh-WilliamsJ@CFBS.Domain.Local>
2026-09-04 14:30:58 +0100

    trying to activate buttons

--- Football Solution/project.py
+++ Football Solution/project.py
@@ -1,25 +1,37 @@
 import pygame
 
-pygame.init()
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Penalty Shootout")
-clock = pygame.time.Clock()
+pygame.init() #initiates pygame module
+screen = pygame.display.set_mode((800, 600)) #establishes the size of the screen to be displayed
 
-while running:
+pygame.display.set_caption("Penalty Shootout") #establishes the text to be displayed as the caption of the screen
+
+clock = pygame.time.Clock() #creates a clock object which keeps track of time
+running = True #running set to true
+
+
+titlePanel = pygame.display.set_mode((200, 100))
+font = pygame.font.Font('freesansbold.ttf', 32)
+titleMessage = font.render("Final Penalty Shootout", True, (0, 255, 0), (0, 0, 128))
+titleRect = titleMessage.get_rect()
+titleRect.center = (400, 300)
+
+
+
+
+while running: #as long as running is true
     # poll for events
     # pygame.QUIT event means the user clicked X to close your window
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False
+            running = False #running is changed to False, meaning this loop stops
 
     # fill the screen with a color to wipe away anything from last frame
     screen.fill("white")
 
-    pygame.draw.circle(screen, "red", player_pos, 40)
-
+    titlePanel.blit(titleMessage, titleRect)
 
     # flip() the display to put your work on screen
-    pygame.display.flip()
+    pygame.display.update()
 
     # limits FPS to 60
     # dt is delta time in seconds since last frame, used for framerate-
@@ -30,6 +42,6 @@ while running:
 
 
 
-
 pygame.quit()
 quit()
+pygame.display.update()
