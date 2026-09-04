003c514d23e2c3690bff016e71c3a07c0aa6448c
Jamhmai Nandoh-Williams <25Nandoh-WilliamsJ@CFBS.Domain.Local>
2026-09-04 12:41:34 +0100

    separated pygame and classes, trying to work pygame

--- Football Solution/classes.py
+++ Football Solution/classes.py
@@ -1,42 +1,3 @@
-import pygame
-
-pygame.init()
-screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Penalty Shootout")
-clock = pygame.time.Clock()
-
-while running:
-    # poll for events
-    # pygame.QUIT event means the user clicked X to close your window
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    # fill the screen with a color to wipe away anything from last frame
-    screen.fill("white")
-
-    pygame.draw.circle(screen, "red", player_pos, 40)
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 500 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 500 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 500 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 500 * dt
-
-    # flip() the display to put your work on screen
-    pygame.display.flip()
-
-    # limits FPS to 60
-    # dt is delta time in seconds since last frame, used for framerate-
-    # independent physics.
-    dt = clock.tick(60) / 1000
-
-
-
 class Player():
     #private username
     #private HighScore
@@ -142,8 +103,3 @@ class ViewScorePanel():
     #private buttons
     #private background
     None
-
-
-
-pygame.quit()
-quit()
---
+++ Football Solution/project.py
@@ -0,0 +1,35 @@
+import pygame
+
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Penalty Shootout")
+clock = pygame.time.Clock()
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("white")
+
+    pygame.draw.circle(screen, "red", player_pos, 40)
+
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
+
+
+
+
+
+
+pygame.quit()
+quit()
