3580a417a917e58cf462199b63cea576c075c6f9
Jamhmai Nandoh-Williams <25Nandoh-WilliamsJ@CFBS.Domain.Local>
2026-09-04 12:37:24 +0100

    working around pygame

--- pygame_testing/project.py
+++ pygame_testing/project.py
@@ -1,11 +1,39 @@
 import pygame
 
-# pygame.init()
-# WIDTH, HEIGHT = 800, 600
-# screen = pygame.display.set_mode((WIDTH, HEIGHT))
-# pygame.display.set_caption("Penalty Shootout")
-# clock = pygame.time.Clock()
+pygame.init()
+screen = pygame.display.set_mode((800, 600))
+pygame.display.set_caption("Penalty Shootout")
+clock = pygame.time.Clock()
 
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    # fill the screen with a color to wipe away anything from last frame
+    screen.fill("white")
+
+    pygame.draw.circle(screen, "red", player_pos, 40)
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_w]:
+        player_pos.y -= 500 * dt
+    if keys[pygame.K_s]:
+        player_pos.y += 500 * dt
+    if keys[pygame.K_a]:
+        player_pos.x -= 500 * dt
+    if keys[pygame.K_d]:
+        player_pos.x += 500 * dt
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    # limits FPS to 60
+    # dt is delta time in seconds since last frame, used for framerate-
+    # independent physics.
+    dt = clock.tick(60) / 1000
 
 
 
@@ -60,8 +88,11 @@ class PenaltyTaker():
 class Goalkeeper():
     #private background
     #private totalScore
-    #private diveButton
     #private diveDirection
+
+    def __init__(self, theTotalScore, theDiveDirection):
+        self.totalScore = theTotalScore
+        self.theDiveDirection = theDiveDirection
 
 
 class MainGame():
@@ -69,7 +100,7 @@ class MainGame():
     #private background
     #private totalScore
     #private 
-None
+    None
 
 
 class Button():
@@ -78,7 +109,7 @@ class Button():
     #private y
     #private width
     #private height
-None
+    None
 
 
 
@@ -86,27 +117,33 @@ None
 class MainMenuPanel():
     #private buttons
     #private background
-
+    None
 
 
 class SettingsPanel():
     #private buttons
     #private background
+    None
 
 
 class CustomisationPanel():
     #private buttons
     #private background
+    None
+
 
 class InstructionsPanel():
     #private buttons
     #private background
+    None
 
 
 class ViewScorePanel():
     #private buttons
     #private background
+    None
 
 
 
 pygame.quit()
+quit()
