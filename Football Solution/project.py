8426324420b991de267b17ce9ab29f97403a2307
unknown <2525Nandoh-WilliamsJ@centralfoundationboys.co.uk>
2026-09-08 22:30:10 +0100

    button design/functionality

--- Football Solution/project.py
+++ Football Solution/project.py
@@ -13,19 +13,19 @@ titlePanel = pygame.Surface((200, 600)) #creates a new surface object with the s
 font = pygame.font.Font('freesansbold.ttf', 50) #creates a font object with the specified font file and size
 titleMessage = font.render("Final Penalty Shootout", True, 	(18, 70, 45), (255, 215, 60)) #creates a text message to be displayed on the screen, with the text "Final Penalty Shootout", in colour (18, 70, 45) with a background colour of (255, 215, 60)
 titleRect = titleMessage.get_rect() #this gets the rectangular area of the title message
-titleRect.center = (400, 50) #coordinates of the center of the title message
+titleRect.center = (500, 50) #coordinates of the center of the title message
 
 mainMenuPanel = pygame.Surface((400, 300)) #creates a new surface object with the specified size (400, 300) which can be used to draw on and display on the screen
-font = pygame.font.Font('freesansbold.ttf', 32) #creates a font object with the specified font file and size
+font = pygame.font.Font('freesansbold.ttf', 40) #creates a font object with the specified font file and size
 mainMenuMessage = font.render("Main Menu", True, (0, 255, 0), (255, 255, 255)) #creates a text message to be displayed on the screen, with the text "Main Menu", in green colour (0, 255, 0) with a background colour of blue (0, 0, 128)
 mainMenuRect = mainMenuMessage.get_rect() #this gets the rectangular area of the main menu message
-mainMenuRect.center = (400, 150) #coordinates of the center of the main menu message
+mainMenuRect.center = (500, 150) #coordinates of the center of the main menu message
 
 settingsPanel = pygame.Surface((400, 200)) #creates a new surface object with the specified size (400, 200) which can be used to draw on and display on the screen
 font = pygame.font.Font('freesansbold.ttf', 32) #creates a font object with the specified font file and size
 settingsMessage = font.render("Settings", True, (0, 255, 0), (255, 255, 255)) #creates a text message to be displayed on the screen, with the text "Settings", in green colour (0, 255, 0) with a background colour of blue (0, 0, 128)
 settingsRect = settingsMessage.get_rect() #this gets the rectangular area of the settings message
-settingsRect.center = (400, 300) #coordinates of the center of the settings message
+settingsRect.center = (500, 300) #coordinates of the center of the settings message
 
 
 while running: #as long as running is true
