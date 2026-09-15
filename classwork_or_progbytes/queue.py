c8615d685a2f527ca6d9cbba856ab1119f49991d
unknown <2525Nandoh-WilliamsJ@centralfoundationboys.co.uk>
2026-09-15 21:42:59 +0100

    queues

--- classwork_or_progbytes/queue.py
+++ classwork_or_progbytes/queue.py
@@ -1,3 +1,19 @@
 q = ["Sarah", "Charles", "Bob", "Melissa"]
 
-    
+maxSize = 4
+
+def enqueue(item, q):
+    if not isFull(q):
+        q.append(item)
+
+def dequeue(q):
+    return q.pop(0)
+
+def isFull(q):
+    if len(q) == maxSize:
+        return True
+
+def isEmpty(q):
+    if len(q) < 1:
+        return True
+
