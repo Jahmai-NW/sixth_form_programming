68a4a636d36a344922292d983570f67976599001
Jamhmai Nandoh-Williams <25Nandoh-WilliamsJ@CFBS.Domain.Local>
2026-09-18 14:30:35 +0100

    why is B last??????

--- classwork_or_progbytes/19.09.2026.py
+++ classwork_or_progbytes/19.09.2026.py
@@ -1,6 +1,8 @@
 teams = ["Arsenal", "Chelsea", "Crystal Palace", "Manchester United", "Liverpool", "Manchester City", "Everton", "Spurs", "Hull", "Brighton"]
 points = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
+
+
 def pointsAssign():
     for i in range(len(points)):
         assign = int(input("Please enter total points for each team:" ))
@@ -10,17 +12,43 @@ def pointsAssign():
 def leagueWinner():
     i = 0
     topScore = points[i]
-    
-    for i in range(len(points)-1):
-        if points[i+1] >= topScore:
-            topScore = points[i+1]
 
-    print(topScore)
+    for i in range(len(points)-1):
+        if points[i-1] >= topScore:
+            topScore = points[i-1]
+            team = teams[i-1]
+    print(team + ": " + str(topScore) + "pts")
+
+
+def alphaSort():
+    for i in range(1, len(teams)):
+        holder = teams[i]
+        pos = i-1
+        while teams[pos] > holder and pos >= 0:
+            teams[pos+1] = teams[pos]
+            pos = pos - 1
+        teams[pos+1] = holder
+
+def pointsSort():
+    for i in range(1, len(points)):
+        holder = points[i]
+        pos = i-1
+        while teams[pos] > holder and pos >= 0:
+            points[pos+1] = points[pos]
+            pos = pos - 1
+        points[pos+1] = holder
+        
+        
+        
+        
+        
+
+            
 
 
 print(teams)
 pointsAssign()
-leagueWinner()
+
 
 while True:
 
@@ -29,25 +57,21 @@ while True:
     1. Sort alphabetically
     2. Sort by points
     3. Find out league winner
-    4. Display Leaderboard
-    5. Exit
+    4. Exit
     ''')
 
     choice = int(input("Please enter the required service: "))
 
     if choice == 1:
-        None
+        alphaSort()
 
     if choice == 2:
-        None
+        pointsSort()
 
     if choice == 3:
-        None
+        leagueWinner()
 
     if choice == 4:
-        None
-
-    if choice == 5:
         quit()
 
 
